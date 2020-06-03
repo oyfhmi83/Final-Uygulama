d5c1d55957f5b0c0e44bb3373b2638e964fc317f
oyfhmi83 <66204886+oyfhmi83@users.noreply.github.com>
2020-06-03 10:07:40 +0300

    Add files via upload

---
+++ Final uygulama 1/Uygulama4/src/uygulama4/Uygulama4.java
@@ -0,0 +1,20 @@
+﻿
+package uygulama4;
+
+import java.util.Scanner; 
+public class Uygulama4 {
+
+    public static void main(String[] args) 
+    {
+     int sayi,sonuc;  
+        Scanner input=new Scanner(System.in); 
+System.out.print( "Lütfen Bir Sayı Giriniz:" ); 
+        sayi=input.nextInt(); 
+        sonuc=kup(sayi); // 
+System.out.println(sayi+ "Sayının Küpü = " + sonuc); 
+    }
+        public static int kup(int a) 
+        {
+                    return a*a*a;
+        }
+}
