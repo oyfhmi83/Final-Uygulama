84a4549ef277f8426a119ae70838170db0bb6434
oyfhmi83 <66204886+oyfhmi83@users.noreply.github.com>
2020-06-03 10:12:45 +0300

    Update Uygulama4.java

--- Final uygulama 1/Uygulama4/src/uygulama4/Uygulama4.java
+++ Final uygulama 1/Uygulama4/src/uygulama4/Uygulama4.java
@@ -1,20 +1,20 @@
 ﻿
 package uygulama4;
 
-import java.util.Scanner; 
+import java.util.Scanner; //Kullanıcının girdiği değerleri okuyabilmek için util dahil ediyorum.
 public class Uygulama4 {
 
     public static void main(String[] args) 
     {
-     int sayi,sonuc;  
-        Scanner input=new Scanner(System.in); 
-System.out.print( "Lütfen Bir Sayı Giriniz:" ); 
+     int sayi,sonuc;  //int değişkeninde parametre alıyoruz
+        Scanner input=new Scanner(System.in); //Kullanıcının girdiği değerleri okumak için Scanner kullanıyoruz.
+System.out.print( "Lütfen Bir Sayı Giriniz:" ); //Kullanıcıdan alınan sonucu ekrana yazdırdım
         sayi=input.nextInt(); 
         sonuc=kup(sayi); // 
-System.out.println(sayi+ "Sayının Küpü = " + sonuc); 
+System.out.println(sayi+ "Sayının Küpü = " + sonuc); //Kullanıcıdan alınan sonucu ekrana yazdırdım
     }
-        public static int kup(int a) 
+        public static int kup(int a) //Alınan sonucun değerini alıp küp sayısını buluyoruz.
         {
-                    return a*a*a;
+                    return a*a*a; //Değeri döndürüp işlemi sonlandırır.
         }
 }
